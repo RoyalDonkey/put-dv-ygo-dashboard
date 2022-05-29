f6d07351004c3b817df5b27ca246ec5e29f40e4e
Randoragon <randoragongamedev@gmail.com>
2022-05-29 20:34:05 +0200

    Implement basic UI and server boilerplate
    The structure for everything seems laid down pretty nicely. All that's
    left is to implement every plot (easier said than done) and paste it
    into the server-side. Then probably play around some more with CSS and
    do some theming.

--- Yu-Gi-Oh-Dashboard/server.R
+++ Yu-Gi-Oh-Dashboard/server.R
@@ -19,18 +19,20 @@ getCard <- function(id) {
   return(cards[cards$id == id,])
 }
 
-# Define server logic required to draw a histogram
+# Define server logic
 shinyServer(function(input, output) {
 
-    output$distPlot <- renderPlot({
-
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white')
-
+    output$PLT_PopularCards <- renderPlot({
+      # TODO
     })
-
+    
+    output$PLT_AtkDefStats <- renderPlot({
+      # TODO
+    })
+    
+    output$PLT_DeckBreakdown <- renderPlot({
+      # TODO
+    })
+    
+    # TODO (Meme Counters)
 })
--- Yu-Gi-Oh-Dashboard/ui.R
+++ Yu-Gi-Oh-Dashboard/ui.R
@@ -9,25 +9,64 @@
 
 library(shiny)
 
+# Constants
+IMG_TITLE_SRC <- 'https://cdn3.iconfinder.com/data/icons/7-millennium-items/512/Milennium_Puzzle_Icon_Colored-512.png'
+
 # Define UI for application that draws a histogram
 shinyUI(fluidPage(
 
-    # Application title
-    titlePanel("Yu-Gi-Oh! Duel Monsters Dashboard"),
+    # Inline custom CSS
+    tags$head(
+      tags$style(HTML('
+      /* Tweak alignment of the title text relative to title image */
+      #TITLE {
+          vertical-align: bottom;
+      }
+      
+      /* Override navbar width and height */
+      .col-sm-4 {
+          width: auto;
+          margin-right: 20px;
+          height: 100vh;
+      }
+      '))
+    ),
+  
+    # Dashboard title
+    HTML('<h1>'),
+    img(src=IMG_TITLE_SRC, width=48),
+    span('Yu-Gi-Oh! Duel Monsters Dashboard', id='TITLE'),
+    HTML('</h1>'),
 
-    # Sidebar with a slider input for number of bins
-    sidebarLayout(
-        sidebarPanel(
-            sliderInput("bins",
-                        "Number of bins:",
-                        min = 1,
-                        max = 50,
-                        value = 30)
-        ),
-
-        # Show a plot of the generated distribution
-        mainPanel(
-            plotOutput("distPlot")
+    # Body
+    navlistPanel(
+        tabPanel('Home Page', value='NAV_HomePage', icon=icon('home'),
+                 h2('Home Page'),
+                 p('Welcome to the Yu-Gi-Oh! Duel Monsters Dashboard!')
+                 ),
+        tabPanel('Popular Cards', value='NAV_PopularCards', icon=icon('star'),
+                 # TODO
+                 h2('Popular Cards'),
+                 p('<brief description of the plot and how to use it>'),
+                 plotOutput('PLT_PopularCards')
+                 ),
+        tabPanel('ATK/DEF Stats', value='NAV_AtkDefStats', icon=icon('shield-alt'),
+                 # TODO
+                 h2('ATK/DEF Stats'),
+                 p('<brief description of the plot and how to use it>'),
+                 plotOutput('PLT_AtkDefStats')
+                 ),
+        tabPanel('Deck Breakdown', value='NAV_DeckBreakdown', icon=icon('chart-pie'),
+                 # TODO
+                 h2('Deck Breakdown'),
+                 p('<brief description of the plot and how to use it>'),
+                 plotOutput('PLT_DeckBreakdown')
+                 ),
+        tabPanel('Meme Counters', value='NAV_MemeCounters', icon=icon('ankh'),
+                 # TODO
+                 h2('Meme Counters')
+                 # not sure what exactly will end up here yet
+                 ),
         )
     )
-))
+)
